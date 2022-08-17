c61dcda7621540639aaa68b5a9555682c38f5168
kishanjvns <kishanjvns@hotmail.com>
2022-08-17 15:47:31 +0530

    first commit

---
+++ src/main/java/com/tech/kj/EurekaServerReg1Application.java
@@ -0,0 +1,15 @@
+package com.tech.kj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerReg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerReg1Application.class, args);
+	}
+
+}
